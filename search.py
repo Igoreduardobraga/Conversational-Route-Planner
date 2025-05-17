e7e8b7104e0ecae39b7531b1f7ad2e0d6cfbc3e0
igoreduardobraga <igoredubraga@gmail.com>
2025-05-17 19:46:05 -0300

    feat: add bfs and astar methods

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import networkx as nx
 
 place_name = "Belo Horizonte, Minas Gerais, Brasil"
 
-filter = '["highway"~"primary|secondary|tertiary"]'
+filter = '["highway"~"primary|secundary|tertiary"]'
 
 G = ox.graph_from_place(place_name, network_type="walk", custom_filter=filter)
 
@@ -18,46 +18,41 @@ def shortest_path(source, target):
     source_node = ox.distance.nearest_nodes(G, source_point[1], source_point[0])
     target_node = ox.distance.nearest_nodes(G, target_point[1], target_point[0])
     
-    route = nx.shortest_path(G, source_node, target_node, weight="length")
+    bfs_route = bfs_path(G, source_node, target_node)
+    a_star_route = a_star_path(G, source_node, target_node)
     
-    return route
+    return bfs_route, a_star_route
 
-def plot_route(route):
-    fig, ax = ox.plot_graph_route(G, route, route_linewidth=4, node_size=0)
-    plt.show()
+def bfs_path(G, source, target):
+    try:
+        return nx.shortest_path(G, source, target)
+    except nx.NetworkXNoPath:
+        return None
     
+def a_star_path(G, source, target):
+    try:
+        return nx.astar_path(G, source, target, weight='length')
+    except nx.NetworkXNoPath:
+        return None
 
-def route_street_names(G, route):
-    street_names = []
-    for u, v in zip(route[:-1], route[1:]):
-        edge_data = G.get_edge_data(u, v, 0)
+def plot_routes(route1, route2):
+    fig, ax = ox.plot_graph(G, show=False, close=False)
 
-        if edge_data is None:
-            continue
-
-        name = edge_data.get('name', None)
-
-        if isinstance(name, list):
-            name = ", ".join(name)
-        if name is None:
-            name = "Unnamed Road"
-
-        if not street_names or street_names[-1] != name:
-            street_names.append(name)
-
-    return street_names
+    # Plotar rota BFS em azul
+    if route1:
+        ox.plot_graph_route(G, route1, route_linewidth=4,
+                            route_color='blue', ax=ax, show=False, close=False)
+    # Plotar rota A* em vermelho
+    if route2:
+        ox.plot_graph_route(G, route2, route_linewidth=4,
+                            route_color='red', ax=ax, show=True, close=True)
 
 
 def format_route(route):
     if isinstance(route, str):
         return route
 
-    streets = route_street_names(G, route)
-    n_streets = len(streets)
+    coords = [(G.nodes[n]['y'], G.nodes[n]['x']) for n in route]
+    coords_str = ", ".join([f"({y:.5f}, {x:.5f})" for y, x in coords])
 
-    if n_streets == 0:
-        return "No street names found on the route."
-
-    streets_str = " -> ".join(streets)
-
-    return f"Route with {len(streets)} street segments: {streets_str}"
+    return f"Path with {len(coords)} points: {coords_str}"
