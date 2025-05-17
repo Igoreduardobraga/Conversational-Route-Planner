b0739ea31f324b4f577390f26d8816a73407dfd8
igoreduardobraga <igoredubraga@gmail.com>
2025-05-17 16:58:08 -0300

    feat: add initial agent

---
+++ agent.py
@@ -0,0 +1,31 @@
+from smolagents import CodeAgent, LiteLLMModel, Tool, FinalAnswerTool
+
+
+class FindRouteTool(Tool):
+    name = "find_route"
+    description = "This tool must be used when you want to find the route or path between two places, locations or points."
+
+    inputs = {
+        "source": {
+            "type": "string",
+            "description": "The source location. It can be an address, a place, a street name, etc."
+        },
+        "target": {
+            "type": "string",
+            "description": "The target location. It can be an address, a place, a street name, etc."
+        }
+    }
+    output_type = "string"
+
+    def forward(self, source, target):
+        return f"The shortest path from {source} to {target} is ..."
+
+
+model = LiteLLMModel(
+    model_id="ollama/qwen3:4b",
+    api_base="http://localhost:11434",
+    max_ctx=8192,
+)
+
+agent = CodeAgent(tools=[FindRouteTool()], model=model, add_base_tools=False)
+agent.run("How can I go from Margô Drinkeria to Berilo Cozinha & Drinks")
